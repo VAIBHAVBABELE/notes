d327a22d052fdd7516774e58a4e4f156e211ed1e
VAIBHAV BABELE <vaibhavbabele15@gmail.com>
2025-11-03 12:49:15 +0530

    add some files

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    use HasFactory;
+
+    // ADD THIS - Mass assignment allow karega
+    protected $fillable = ['name', 'description'];
+
+    // Relationship with resources
+    public function resources()
+    {
+        return $this->hasMany(Resource::class);
+    }
 }
